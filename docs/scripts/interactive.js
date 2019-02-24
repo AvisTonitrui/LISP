7fb40c92b73556bafd41444a36dbf067b457470c
NeaFotia <neopolitan421@gmail.com>
2019-02-23 16:39:36 -0800

    Working on the interactive experience.

--- docs/scripts/interactive.js
+++ docs/scripts/interactive.js
@@ -2,7 +2,7 @@
 graphic = document.getElementById("graphic");
 option1 = document.getElementById("option1");
 option2 = document.getElementById("option2");
-option3 = document.getElementById("option3");
+nextButton = document.getElementById("next-button");
 dialogueText = document.getElementById("dialogue-text");
 
 //what number of the dialogue options we are currently on
@@ -10,20 +10,62 @@ dialogueNum = 0;
 
 //the giant list of dialogue options
 dialogueOptions = [
-		"Lorem Ipsum",
-		"Ipsum Lorem"
+		[false, "Lorem Ipsum", "", ""],
+		[true, "Ipsum Lorem", '<button type="button" onclick=\'optionChoice(true, "")\'>Test</button>', "<button type=\"button\" onclick=\"optionChoice(false, failTexts[0])\">Test</button>"],
+		[false, "You chose correctly", "", ""]
+	];
+
+//all of the failure dialogues
+failTexts = [
+		"You chose incorrectly"
 	];
 
 //pulls up new dialogue and options if applicable
-function newDialogue(option, options) {
-	dialogueText.innerHTML = dialogueOptions[dialogueNum];
+function newDialogue(option) {
+	//set the new dialogue
+	dialogueText.innerHTML = dialogueOptions[dialogueNum][1];
 	
-	if (option) {
-		option1.innerHTML = options[0];
-		option2.innerHTML = options[1];
-		option3.innerHTML = options[2];
+	//set up the options
+	console.log("Should be changing the options");
+	console.log(dialogueOptions[dialogueNum][2]);
+	console.log(dialogueOptions[dialogueNum][3]);
+	option1.innerHTML = dialogueOptions[dialogueNum][2];
+	option2.innerHTML = dialogueOptions[dialogueNum][3];
+	
+	//set up the next button for the next dialogue option
+	if(option) {
+		nextButton.innerHTML = "<button type=\"button\" disabled>Next</button>";
+	} else {
+		nextButton.innerHTML = "<button type=\"button\" onclick=\"newDialogue(" + dialogueOptions[dialogueNum + 1][0] + ")\">Next</button>";
 	}
 	
 	//increment the dialogue number
 	dialogueNum += 1;
+}
+
+function optionChoice(correct, failText) {
+	//if correct, proceed with the story
+	if (correct) {
+		//set the new dialogue
+		dialogueText.innerHTML = dialogueOptions[dialogueNum][1];
+		
+		//clear the options
+		option1.innerHTML = "";
+		option2.innerHTML = "";
+		
+		//set up the next button for the next dialogue option
+		nextButton.innerHTML = "<button type=\"button\" onclick=\"newDialogue(" + dialogueOptions[dialogueNum + 1][0] + ")\">Next</button>";
+		
+		//increment the dialogue number
+		dialogueNum += 1;
+	}
+	//otherwise we give the fail text
+	else {
+		//set the fail dialogue
+		dialogueText = failText;
+		
+		//clear the options
+		option1.innerHTML = "";
+		option2.innerHTML = "";
+	}
 }
